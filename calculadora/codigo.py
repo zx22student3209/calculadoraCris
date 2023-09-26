da672d2a5323a85cd1e7516bca1dc8185c06916a
zx22student3209 <zx22student3209@colegiovalledelmiro.es>
2023-09-26 21:32:06 +0200

    funcionDividir

--- calculadora/codigo.py
+++ calculadora/codigo.py
@@ -11,6 +11,10 @@ def sumar():
     c = (x + y)
     print(c)
 
+def dividir():
+    c = x / y
+    return c
+
 print("Seleccione una operacion \n 1-Sumar \n 2-restar \n 3-Multiplicacion \n")
 
 s=input()
