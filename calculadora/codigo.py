2e03c48a1d203a5a0125fde665850ff42806fcb9
zx22student3209 <zx22student3209@colegiovalledelmiro.es>
2023-09-26 10:15:55 +0200

    Primera version calculadora; Sumar

---
+++ calculadora/codigo.py
@@ -0,0 +1,16 @@
+#!C:/Users/zx22student3209/AppData/Local/Programs/Python/Python311/python.exe
+
+print("Content-Type: text/html\n")
+
+x = input("Ingresa primer numero")
+y = input("Ingresa segundo numero")
+c=0
+
+
+def sumar():
+    c = (x + y)
+    print(c)
+
+print("Seleccione una operacion \n 1-Sumar \n 2-restar \n 3-Multiplicacion \n")
+
+s=input()
